9df96b634ebef9accd1333579c7e7bb6cc3a2a86
ZhangMin1998 <1464253423@qq.com>
2023-06-29 01:31:58 +0800

    px to rem

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text">111</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style lang="scss">
+.home{
+  .text{
+    font-size: 2rem;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+const px2rem = require('postcss-px2rem')
+
+const postcss = px2rem({
+  remUnit: 32
 })
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  publicPath: './',
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            // postcss
+            require('postcss-px2rem')({
+              remUnit: 16
+            })
+          ]
+        }
+      }
+    }
+  }
+}
